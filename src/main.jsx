af93f5dd669e72c7f00da40f9662e8a84793c48c
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-10-17 21:13:09 +0500

    setting up routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,58 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import Home from "./pages/Home.jsx"
+import Login from "./pages/Login.jsx"
+import Signup from "./pages/Signup.jsx"
+import AllPosts from "./pages/AllPost.jsx"
 
-createRoot(document.getElementById('root')).render(
+import AuthLayout from "./components/AuthLayout.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            <AllPosts/>
+          </AuthLayout>
+        )
+      },
+    ]
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
