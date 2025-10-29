ed78ca475a8c6b7910e06ffc046d39e60695ce0a
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-10-29 18:23:28 +0500

    creating login and signUp

---
+++ src/components/Login.jsx
@@ -0,0 +1,82 @@
+import { useForm } from "react-hook-form";
+import Button from "./Button";
+import Input from "./input";
+import Logo from "./Logo";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import authService from "../appwrite/auth";
+import { login as authLogin } from "../store/authSlice";
+
+function Login() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.login(data);
+      if (session) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(authLogin({ userData }));
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center w-full">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          SignIn to your account
+        </h2>
+        <p className="mt-2 text-center text-2xl font-bold leading-tight">
+          Don&apos;t have any account?&nbsp;
+          <Link
+            to="/signup"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Sing Up
+          </Link>
+        </p>
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+        <form onSubmit={handleSubmit(login)} className="mt-8">
+          <div className="space-y-5">
+            <Input
+              {...register("email", {
+                required: true,
+              })}
+              label="Email: "
+              placeholder="Email Address"
+              type="email"
+            />
+            <Input
+              {...register("password", {
+                required: true,
+              })}
+              label="Password"
+              type="password"
+              placeholder="Password"
+            />
+            <Button type="submit" className="w-full">
+              Sign In{""}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
